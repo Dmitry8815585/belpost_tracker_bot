139206bb115be564b7f123fc6511b43a711253b5
Dmitry Mikhaylov <good8815585@yandex.ru>
2024-02-15 18:48:16 +0300

    Checking done

---
+++ blblblblblb.py
@@ -0,0 +1,10 @@
+from time import sleep
+
+
+lst = ['one', 'two', 'three']
+
+
+while True:
+    for i in lst:
+        print(i)
+        sleep(2)
--- checking.py
+++ checking.py
@@ -4,55 +4,52 @@ import time
 
 from belpost_request import get_data
 from db_manager import get_chat_id, get_value_from_db, update_track_data
-from main_bot import send_message
+# from main_bot import send_message
 from test_json import data
 
 
 def checking():
-    connection = sqlite3.connect('belpost_tracker.db')
-    try:
+    while True:
+        connection = sqlite3.connect('belpost_tracker.db')
         cursor = connection.cursor()
         cursor.execute(
             'SELECT track FROM tracks WHERE is_active IS NOT FALSE',
         )
         rows = cursor.fetchall()
+
+        if not rows:
+            connection.close()
+            print('No tracks to check. Exiting...')
+            return
+
         print(rows)
 
         for i in rows:
             track = i[0]
 
-            # request_data = data
-            while True:
-                try:
-                    request_data = get_data(track)
+            try:
+                request_data = get_data(track)
+                time.sleep(5)
+                if request_data != get_value_from_db(track):
+                    if request_data[-1].get('event') == 'Вручено':
+                        update_track_data(track, request_data, False)
+                        print('Вручено')
 
-                    if request_data != get_value_from_db(track):
-                        if request_data[-1].get('event') == 'Вручено':
-                            update_track_data(track, request_data, False)
-                            print('Вручено')
-                            send_message(
-                                chat_id=get_chat_id(track),
-                                message='Вручено!'
-                            )
-
-                        else:
-                            update_track_data(track, request_data, True)
-                            print(f'Data for {track} has changed')
                     else:
-                        print(f'No new data for {track}')
-                        time.sleep(60)
-                        continue
+                        update_track_data(track, request_data, True)
+                        print(f'Data for {track} has changed')
+                else:
+                    print(f'No new data for {track}')
 
-                except Exception as e:
-                    print(
-                        f"An error occurred while getting data for {track}: {e}"
-                    )
-                    time.sleep(300)
-    except Exception as e:
-        print("An error occurred:", e)
-    finally:
+            except Exception as e:
+                print(
+                    f"An error occurred while getting data for {track}: {e}"
+                )
+                time.sleep(5)
         connection.close()
-        print('All done!')
+        print('Loop done!')
+        time.sleep(5)
+        continue
 
 
 def main():
